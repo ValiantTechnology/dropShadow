75664124e4ac1ec045bf9e59c50f6eeb3a904d84
Matthew F. Fox <matt@100781.org>
2017-10-31 12:18:35 -0400

    Update dropshadow.sh

--- dropshadow.sh
+++ dropshadow.sh
@@ -3,10 +3,8 @@ while true; do
     read -p "Do you want OSX screenshots to include a drop shadow? 
 (Y/N)" yn
     case $yn in
-        [Yy]* ) defaults write com.apple.screencapture disable-shadow 
--bool false; killall SystemUIServer; break; ;;
-        [Nn]* ) defaults write com.apple.screencapture disable-shadow 
--bool true; killall SystemUIServer; break; ;;
+        [Yy]* ) defaults write com.apple.screencapture disable-shadow -bool false; killall SystemUIServer; break; ;;
+        [Nn]* ) defaults write com.apple.screencapture disable-shadow -bool true; killall SystemUIServer; break; ;;
         * ) echo "Please answer Y or N."; ;;
     esac
 done
